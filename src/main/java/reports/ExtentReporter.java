f7576ac03d6dc42b4042bc54888a12649f8ad851
kursSelenium <kursselenium@interia.pl>
2020-05-03 18:20:38 +0200

    fix2

--- src/main/java/reports/ExtentReporter.java
+++ src/main/java/reports/ExtentReporter.java
@@ -24,7 +24,7 @@ public class ExtentReporter  implements IReporter {
 
     public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
                                String outputDirectory) {
-        extent = new ExtentReports(outputDirectory + File.separator
+        extent = new ExtentReports("test-output/"
                 + "RaportZTestow.html", true);
 
         extent
